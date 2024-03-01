3ae65d771f99dee1d5dd31287407b9b6e6646230
Jonas-Kunz <jonas_K_7@outlook.com>
2024-03-01 16:53:55 -0400

    complete ruby benchmark

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
+  start_time = Time.now
+  yield
+  finish_time = Time.now
   # Your benchmarking code goes here.
+  return finish_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
